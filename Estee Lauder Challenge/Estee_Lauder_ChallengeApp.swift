ca1e5d967f2489d974743f40795b1dabfe121514
Jude Shorbaji <jshorbaji@hillers.org>
2023-07-04 14:38:13 -0400

    Initial Commit

---
+++ Estee Lauder Challenge/Estee_Lauder_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_Lauder_ChallengeApp.swift
+//  Estee Lauder Challenge
+//
+//  Created by Jude Shorbaji on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_Lauder_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
