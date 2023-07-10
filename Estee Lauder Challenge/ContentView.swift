af370484f2a49c3555c7d6ddfc90cff9ae120a95
Jude Shorbaji <jshorbaji@hillers.org>
2023-07-10 16:36:24 -0400

    Iteration 1- addition of information page and partial UI overhaul

--- Estee Lauder Challenge/ContentView.swift
+++ Estee Lauder Challenge/ContentView.swift
@@ -9,53 +9,120 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{  
-            VStack {
-                HStack{
-                    Text("Welcome Guest")
-                    Spacer()
+        NavigationStack{
+            ZStack{
+                Rectangle()
+                    .frame(width: 500.0, height: 900.0)
+                    .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                VStack(alignment: .center) {
                     Text("Estee Lauder")
-                }
-                .padding(.all, 10.0)
-                Image("estee 49")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.all, 10.0)
-                    .frame(width: 500.0)
-                 
-                Text("Nutritious collection")
-                    .padding(.all, 10.0)
-                HStack{
-                    Image("estee 19")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100.0, height: 100.0)
-                    Image("estee 20")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100.0, height: 100.0)
-                    Image("estee 17")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100.0, height: 100.0)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                            .stroke(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137), lineWidth: 2)
+                            .frame(width: 375.0, height: 40.0)
+                            
+                        HStack{
+                            Text("🔎 search cosmetics")
+                                .foregroundColor(Color(red: 0.82, green: 0.082, blue: 0.255))
+                            Spacer()
+                           
+                        }
+                        .padding(.all, 5.0)
+                    }
                     
-                }
-                .padding(.all, 10.0)
-                NavigationLink(destination: quiz()) {
-                    Text("Find the perfect product ")
+                    Image("estee 49")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 500.0)
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                            .frame(height: 50.0)
+                            .foregroundColor(Color(red: 0.82, green: 0.082, blue: 0.255))
+                        NavigationLink(destination: quiz()) {
+                            Text("Find Your Perfect Product ")
+                                .padding(.all, 20.0)
+                                .foregroundColor(Color.white)
+                        }
+                    }
+                    ZStack{
+                        
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                            .stroke(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137), lineWidth: 2)
+                            .frame(width: 375, height: 200.0)
+                        VStack{
+                            Text("Nutritious collection")
+                                .padding(.all, 10.0)
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                            Spacer()
+                            HStack{
+                                NavigationLink(destination:MoisturizerInfo()){
+                                    ZStack{
+                                        RoundedRectangle(cornerRadius:10)
+                                            .frame(width: 110.0, height: 150.0)
+                                            .foregroundColor(Color(red: 0.922, green: 0.898, blue: 0.886))
+                                        VStack{
+                                            Image("estee 19")
+                                                .resizable(resizingMode: .stretch)
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 100.0, height: 100.0)
+                                            Text("product")
+                                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                                        }
+                                    }
+                                }
+                                NavigationLink(destination:MoisturizerInfo())
+                                {
+                                    ZStack{
+                                        RoundedRectangle(cornerRadius:10)
+                                            .frame(width: 110.0, height: 150.0)
+                                            .foregroundColor(Color(red: 0.922, green: 0.898, blue: 0.886))
+                                        VStack{
+                                            Image("estee 20")
+                                                .resizable(resizingMode: .stretch)
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 100.0, height: 100.0)
+                                            Text("product")
+                                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                                        }
+                                    }
+                                }
+                                NavigationLink(destination: MoisturizerInfo()) {
+                                    
+                                    
+                                    ZStack{
+                                        RoundedRectangle(cornerRadius:10)
+                                            .frame(width: 110.0, height: 150.0)
+                                            .foregroundColor(Color(red: 0.922, green: 0.898, blue: 0.886))
+                                        VStack{
+                                            Image("estee 17")
+                                                .resizable(resizingMode: .stretch)
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 100.0, height: 100.0)
+                                            Text("product")
+                                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                                        }
+                                        
+                                    }
+                                }
+                                
+                            }
+                            Spacer()
+                        }
+                        
                         .padding(.all, 10.0)
+                    }
+                    .frame(height: 200.0)
+                    Spacer()
+                   
                 }
-                Spacer()
-                HStack{
-                    Text("🔎")
-                    Text("👤")
-                    Text("⚙️")
-                }
-                }
+               
+                .frame(width: 375.0, height: 750.0)
             }
-            .padding()
+            
         }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
--- Estee Lauder Challenge/quiz.swift
+++ Estee Lauder Challenge/quiz.swift
@@ -12,111 +12,179 @@ struct quiz: View {
     @State private var foamCleanser=0
     @State private var QuestionNum=0
     var body: some View {
-        if QuestionNum==0{
-            VStack{
-                Text("lets find your perfect NUTRITIOUS product")
-                Button{
-                    QuestionNum=1
-                }label:{
-                    Text("Lets go")
+        ZStack{
+            RoundedRectangle(cornerRadius:0)
+                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                .frame(width:400,height:900)
+            
+            if QuestionNum==0{
+                VStack{
+                    Text("lets find your perfect NUTRITIOUS product")
+                        .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                    Button{
+                        QuestionNum=1
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Lets go")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
                 }
             }
-        }
-        else if QuestionNum==1{
-            VStack{
-                Text("what is your biggest skin goal?")
-                Button{
-                    treatmentLotion+=1
-                }label:{
-                    Text("glow")
-                }
-                Button{
-                    foamCleanser+=1
-                }label:{
-                    Text("Cleanse")
-                }
-                Button{
-                    maskMoisturizer+=1
-                }label:{
-                    Text("Hydrate")
-                }
-                
-                Button{
-                    QuestionNum=2
-                }label:{
-                    Text("Next")
+            else if QuestionNum==1{
+                VStack{
+                    Text("what is your biggest skin goal?")
+                        .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                    Button{
+                        treatmentLotion+=1
+                        QuestionNum=2
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("glow")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                        
+                    }
+                    Button{
+                        foamCleanser+=1
+                        QuestionNum=2
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Cleanse")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                        
+                    }
+                    Button{
+                        maskMoisturizer+=1
+                        QuestionNum=2
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Hydrate")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
+                    
+                    
+                    
                 }
                 
             }
             
-        }
-        
-        
-        //irritated skin, dull skin, large pores
-        
-        else if QuestionNum==2{
-            VStack{
-                Text("do you have any concerns?")
-                Button{
-                    maskMoisturizer+=1
-                }label:{
-                    Text("dullness")
-                }
-                Button{
-                    treatmentLotion+=1
+            
+            //irritated skin, dull skin, large pores
+            
+            else if QuestionNum==2{
+                VStack{
+                    Text("do you have any concerns?")
+                        .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                    Button{
+                        maskMoisturizer+=1
+                        QuestionNum=3
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Dullness")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
+                    Button{
+                        treatmentLotion+=1
+                        QuestionNum=3
                         
-                }label:{
-                    Text("Irritation")
-                }
-                Button{
-                    foamCleanser+=1
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Irritation")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
+                    Button{
+                        foamCleanser+=1
+                        QuestionNum=3
+                        
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Large Pores")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
                     
-                }label:{
-                   Text( "large pores")
-                }
-                Button{
-                    QuestionNum+=1
-                }label:{
-                    Text("next")
-                }
                 }
             }
-        else if QuestionNum==3{
-            VStack{
-                Text("are you a morning bird or a night owl?")
-                Button{
-                    foamCleanser+=1
-                    treatmentLotion+=1
-                }label:{
-                    Text("morning bird")
+            else if QuestionNum==3{
+                VStack{
+                    Text("are you a morning bird or a night owl?")
+                        .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                    Button{
+                        foamCleanser+=1
+                        treatmentLotion+=1
+                        QuestionNum=4
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Morning Bird")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
+                    Button{
+                        maskMoisturizer+=1
+                        QuestionNum=4
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                                .frame(width:70,height:40)
+                                .foregroundColor(Color(red: 0.937, green: 0.9137254901960784, blue: 0.9411764705882353))
+                            Text("Night owl")
+                                .foregroundColor(Color(red: 0.8196078431372549, green: 0.0784313725490196, blue: 0.2549019607843137))
+                        }
+                    }
+                    
+                    
                 }
-                Button{
-                    maskMoisturizer+=1
-                }label:{
-                    Text("night owl")
+            }
+            else if QuestionNum==4{
+                if (maskMoisturizer>foamCleanser && maskMoisturizer>treatmentLotion){
+                    Text("The mask moisturizer is perfect")
                 }
-                Button{
-                    QuestionNum=4
-                }label:{
-                    Text("Done")
+                else if (foamCleanser>maskMoisturizer && foamCleanser>treatmentLotion){
+                    Text("the foam cleanser is perfect")
                 }
-         
+                else if(treatmentLotion>maskMoisturizer && treatmentLotion>foamCleanser){
+                    Text("the treatment lotion is perfect")
+                }
+                else if(treatmentLotion==foamCleanser){
+                    Text("its a tie between the treetment lotion and the foam cleanser!")
+                }
             }
-        }
-        else if QuestionNum==4{
-            if (maskMoisturizer>foamCleanser && maskMoisturizer>treatmentLotion){
-            Text("The mask moisturizer is perfect")
-            }
-            else if (foamCleanser>maskMoisturizer && foamCleanser>treatmentLotion){
-                Text("the foam cleanser is perfect")
-            }
-            else if(treatmentLotion>maskMoisturizer && treatmentLotion>foamCleanser){
-                Text("the treatment lotion is perfect")
-            }
-            else if(treatmentLotion==foamCleanser){
-                Text("its a tie between the treetment lotion and the foam cleanser!")
-            }
-        }
         }
     }
+    }
+
+struct quiz_Previews: PreviewProvider {
+    static var previews: some View {
+        quiz()
+    }
+}
 
